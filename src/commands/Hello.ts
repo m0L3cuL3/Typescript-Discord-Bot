dc2ef1800f552db219a6f86fe3b87e01916ee9b6
seanbaang <dev.seanbaang@gmail.com>
2022-07-29 18:51:50 +0800

    added env configuration

--- src/commands/Hello.ts
+++ src/commands/Hello.ts
@@ -1,13 +1,15 @@
-require('dotenv').config()
-import { CommandInteraction, Client, EmbedBuilder } from 'discord.js'
+require('dotenv').config({path: '.env'})
+import { CommandInteraction, Client } from 'discord.js'
 import { Command } from '../Command'
+
+
 
 export const Hello: Command = {
     name: 'hello',
     description: 'Returns a greeting',
     type: 1,
     run: async (client: Client, interaction: CommandInteraction) => {
-        const guild = client.guilds.cache.get(process.env.GUILD_ID)
+        const guild = client.guilds.cache.get('789199594713251870')
 
         let members = await guild?.members.fetch()
         let usernames = members?.map(m => m.user.tag)
